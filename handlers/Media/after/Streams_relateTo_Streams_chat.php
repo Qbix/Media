bde13f220dcc6462fe4b6aa177b05a387db6d675
Greg Magarshak <greg@qbix.com>
2025-06-29 00:12:29 -0400

    Moved Streams/call to Media/call in Media plugin

--- handlers/Media/after/Streams_relateTo_Streams_chat.php
+++ handlers/Media/after/Streams_relateTo_Streams_chat.php
@@ -8,7 +8,7 @@ function Media_after_Streams_relateTo_Streams_chat($params)
 	if ($relatedStream->type == 'Media/webrtc') {
 		$displayName = Users::fetch($relatedStream->publisherId)->displayName();
 		$chatStream->post($relatedStream->publisherId, array(
-			'type' => 'Streams/call',
+			'type' => 'Media/call',
 			'content' => '.',
 			'instructions' => array(
 				'displayName' => $displayName
