21e0bb9d43388c060d4beb81554dbac90f1ca973
Andrey Tepaykin <andrey@qbix.com>
2025-11-11 21:01:37 +0300

    Fixed Media/channel column.

--- handlers/Media/channel/response/column.php
+++ handlers/Media/channel/response/column.php
@@ -19,17 +19,13 @@ function Media_channel_response_column(&$params, &$result)
 	}
 	$image = $stream->iconUrl("400");
 
-	Q_Response::setCommonMetas(compact(
-		'title', 'description', 'keywords', 'image', 'url'
-	));
-
+	Q_Response::setCommonMetas(compact('title', 'description', 'keywords', 'image', 'url'));
 	Q_Response::setSlot('title', $title);
-	Q_Response::addStylesheet("{{Media}}/css/columns/channel.css");
 
-	$channelParams = array_merge(array(
-		'publisherId' => $stream->publisherId,
-		'streamName' => $stream->name
-	), Q::ifset($params, "channelParams", array()));
-
-	return Q::view('Media/column/channel.php', compact('channelParams'));
+	return Q::view('Media/column/channel.php', array(
+		'channelParams' => array(
+			'publisherId' => $stream->publisherId,
+			'streamName' => $stream->name
+		)
+	));
 }
