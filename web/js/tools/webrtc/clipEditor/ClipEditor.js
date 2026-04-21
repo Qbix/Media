6b84c9a273aad78b661db14a1c5f8b31d807d6ee
liubomyr-ua <liubomyr@qbix.com>
2026-04-21 07:40:56 +0300

    WIP: working on fMP4 player/parser; working on timeline

--- web/js/tools/webrtc/clipEditor/ClipEditor.js
+++ web/js/tools/webrtc/clipEditor/ClipEditor.js
@@ -1,4 +1,5 @@
 Q.Media.WebRTC.clipEditor.ClipEditor = function () {
+    const thisInstance = this;
     let _file;
 
     this.player = new Q.Media.WebRTC.clipEditor.MediaPlayer();
@@ -33,13 +34,53 @@ Q.Media.WebRTC.clipEditor.ClipEditor = function () {
     console.log('this.player', this.player)
     editorPlayerView.appendChild(this.player.element);
 
-    const editorTimeline = document.createElement('DIV');
-    editorTimeline.className = 'clip-editor-timeline';
+    container.appendChild(buildTimeline());
 
     const clipTitle = document.createElement('DIV');
     clipTitle.className = 'clip-editor-clip-title';
+    container.appendChild(clipTitle);
 
     function onFileOpen() {
           const file = e.target.files[0];
+    }
+
+    function buildTimeline() {
+        const editorTimeline = document.createElement('DIV');
+        editorTimeline.className = 'clip-editor-timeline';
+        container.appendChild(editorTimeline);
+
+        const clipBounds = document.createElement('DIV');
+        clipBounds.className = 'clip-editor-timeline-clip';
+        editorTimeline.appendChild(clipBounds);
+
+        //let totalDuration = thisInstance.player.parser.totalDuration;
+
+        Q.activate(
+            Q.Tool.setUpElement(
+                clipBounds,
+                "Q/resize",
+                {
+                    move: true,
+                    resize: true,
+                    active: true,
+                    resizeByWheel: false,
+                    //elementPosition: 'fixed',
+                    showResizeHandles: false,
+                    moveWithinArea: 'parent',
+                    allowOverresizing: true,
+                    negativeMoving: false,
+                    keepRatio: false,
+                    onMoving: function () {
+
+                    }
+                }
+            ),
+            {},
+            function () {
+                _allParticipantsListInstance.resizingElementTool = this;
+            }
+        );
+
+        return editorTimeline;
     }
 }
