833c18b81dafbdc7c70685de137b440369487dc7
Liubomyr <liubomyr.bondarchuk@gmail.com>
2026-05-29 07:23:26 +0300

    Removed redundant files

--- handlers/Media/youtubeEpisode/response/live.php
+++
@@ -1,8 +0,0 @@
-<?php
-function Media_clip_response_live ($params) {
-	$stream = Media::getCurrentLiveShow();
-	return array(
-		"publisherId" => $stream->publisherId,
-		"streamName" => $stream->name
-	);
-}
